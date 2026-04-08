0f28593605370f155fbbbb705febde6e43e44b27
DTH235601NgoThiNgocAn <an_dth235601@student.agu.edu.vn>
2026-04-08 10:58:55 +0700

    Sua loi dong 46

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.use((req, res, next) => {
 	
 	// Gán thông báo (lỗi, thành công) vào biến cục bộ
 	res.locals.message = '';
-	if (err) res.locals.message =x '<span class="text-danger">' + err + '</span>';
+	if (err) res.locals.message = '<span class="text-danger">' + err + '</span>';
 	if (msg) res.locals.message = '<span class="text-success">' + msg + '</span>';
 	
 	next();
